61373a9ff42caa37e474d15fa58fc6adfb2a36fc
shiyingwqh <wqhshiying@163.com>
2022-09-21 22:58:46 +0800

    update

--- src/main/java/com/wuqihang/syblog/SyBlogApplication.java
+++ src/main/java/com/wuqihang/syblog/SyBlogApplication.java
@@ -1,7 +1,12 @@
 package com.wuqihang.syblog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+import org.thymeleaf.spring5.context.SpringContextUtils;
 
 @SpringBootApplication
 public class SyBlogApplication {
